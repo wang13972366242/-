2cb6531c6a3bfae85bff90af8337d2eb55de1ab9
wang <wang@wangdemacbook-pro.local>
2016-06-16 09:47:51 +0800

    Initial Commit

---
+++ Order/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Order
+//
+//  Created by wang on 16/6/16.
+//  Copyright © 2016年 Particlestechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
